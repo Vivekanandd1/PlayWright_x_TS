aa1efbce13a150671f88a5309b70d734caf5e9a2
Vivekanand Deshmukh <69782201+Vivekanandd1@users.noreply.github.com>
2026-09-20 14:40:44 +0530

    Update CucumberReporter.ts

--- Utils/CucumberReporter.ts
+++ Utils/CucumberReporter.ts
@@ -1,25 +1,26 @@
-import reporter from "cucumber-html-reporter";
-import * as fs from "fs";
-import * as path from "path";
+// Utils/CucumberReporter.ts
+import reporter from 'cucumber-html-reporter';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const outputDir = path.resolve("reports/cucumber-html");
-if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
-}
+const reportJsonDir = path.resolve('reports/json');
+const reportHtmlDir = path.resolve('reports/html');
+
+fs.mkdirSync(reportJsonDir, { recursive: true });
+fs.mkdirSync(reportHtmlDir, { recursive: true });
 
 reporter.generate({
-    theme: "bootstrap",
-    jsonFile: "reports/json/cucumber-report.json",
-    output: "reports/html/cucumber-report.html",
-    reportSuiteAsScenarios: true,
-    launchReport: false,
-
-    metadata: {
-        "Application": "E-Com",
-        "Environment": "QA",
-        "Browser": "Chrome",
-        "Platform": process.platform
-    }
+  theme: 'bootstrap',
+  jsonFile: path.resolve(reportJsonDir, 'cucumber-report.json'),
+  output: path.resolve(reportHtmlDir, 'cucumber-report.html'),
+  reportSuiteAsScenarios: true,
+  launchReport: false,
+  metadata: {
+    Application: 'E-Com',
+    Environment: 'QA',
+    Browser: 'Chrome',
+    Platform: process.platform,
+  },
 });
 
-console.log(`Cucumber Report Generated: file://${path.resolve("reports/html/cucumber-report.html")}`);
+console.log(`Cucumber Report Generated: file://${path.resolve(reportHtmlDir, 'cucumber-report.html')}`);
