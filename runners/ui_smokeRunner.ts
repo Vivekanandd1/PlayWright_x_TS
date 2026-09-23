e25ebdb75f57df4027303c8ce37779bf7590f9ea
vivekanandd1 <vivekanandd1@live.com>
2026-09-23 07:51:11 +0530

    added fixes

---
+++ runners/ui_smokeRunner.ts
@@ -0,0 +1,22 @@
+import { config } from 'dotenv';
+import { loadConfiguration, runCucumber } from '@cucumber/cucumber/api';
+
+config();
+
+(async () => {
+  const { runConfiguration } = await loadConfiguration({
+    provided: {
+      paths: ['feature/*.feature'],
+      require: ['steps/**/*.ts'],
+      format: ['pretty', 'json:reports/json/cucumber-report.json', 'html:reports/html/cucumber-report.html'],
+      /* Add test tag here to run the Script*/
+      tags: '@smoke',
+    },
+  });
+
+  const { success } = await runCucumber(runConfiguration);
+  console.log()
+  console.log(`Report generated at: file://${process.cwd()}/reports/html/cucumber-report.html`);
+  process.exit(success ? 0 : 1);
+})();
+
