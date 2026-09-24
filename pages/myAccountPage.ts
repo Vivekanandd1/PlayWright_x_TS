7549956954e0f03bf67ddd5b44fb2d1de681f83c
vivekanandd1 <vivekanandd1@live.com>
2026-09-24 15:29:52 +0530

    added diferent timeout

--- pages/myAccountPage.ts
+++ pages/myAccountPage.ts
@@ -19,6 +19,7 @@ export class MyAccountPage extends Base {
     }
 
     public static async clickOnSignInButton() {
+        await fixture.page.waitForLoadState('load');
         await this.waitAndClick(this.elements.loginButton);
     }
    
