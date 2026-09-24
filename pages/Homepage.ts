cb1db713c5fef2f03b0659615fa9dcc91c746c07
Vivekanand Deshmukh <69782201+Vivekanandd1@users.noreply.github.com>
2026-09-24 16:48:29 +0530

    Update Homepage.ts

--- pages/Homepage.ts
+++ pages/Homepage.ts
@@ -32,7 +32,7 @@ export class Homepage extends Base {
   }
 
   public static async sortAtoZ(){
-     await fixture.page.waitForLoadState('load');
+     await fixture.page.waitForLoadState('networkidle');
      await fixture.page.locator(this.elements.sortOptions).selectOption({'value' : 'name,asc'});
   }
 
