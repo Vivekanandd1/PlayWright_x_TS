65162825de1f09c1bdd08770bdc6835886e50ffa
vivekanandd1 <vivekanandd1@live.com>
2026-09-24 17:02:49 +0530

    added wais

--- pages/Homepage.ts
+++ pages/Homepage.ts
@@ -32,7 +32,8 @@ export class Homepage extends Base {
   }
 
   public static async sortAtoZ(){
-     await fixture.page.waitForLoadState('load');
+     await fixture.page.waitForLoadState('networkidle');
+     await this.waitForVisibilty(this.elements.sortOptions, 60000)
      await fixture.page.locator(this.elements.sortOptions).selectOption({'value' : 'name,asc'});
   }
 
